fbd9bc7c3badc56906af253c7e753971201c02a6
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-23 23:59:45 +0500

    first

---
+++ boolean16/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace boolean16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(ReadLine());
+            bool b = a>9 && a<100 && a % 2 == 0;
+            WriteLine(b);
+        }
+    }
+}
